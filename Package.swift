628b297892bf0e7edd127cf599110a2ff1a1e480
Tibor Bödecs <mail.tib@gmail.com>
2020-10-19 12:53:17 +0200

    Leaf tag instead of branch

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor", from: "4.30.0"),
         .package(url: "https://github.com/vapor/fluent", from: "4.0.0"),
         //.package(url: "https://github.com/vapor/leaf", from: "4.0.0"),
-        .package(url: "https://github.com/tib/leaf", .branch("tau")),
+        .package(url: "https://github.com/tib/leaf", from: "4.0.0-rc"),
         .package(url: "https://github.com/vapor/fluent-kit", from: "1.0.0"),
     ],
     targets: [
