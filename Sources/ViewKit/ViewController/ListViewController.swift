df2f78838a41398500d773ebdab91c7a28761cc0
Tibor Bödecs <mail.tib@gmail.com>
2020-11-17 19:56:37 +0100

    updated list controller

--- Sources/ViewKit/ViewController/ListViewController.swift
+++ Sources/ViewKit/ViewController/ListViewController.swift
@@ -9,40 +9,49 @@
 public protocol ListViewController: ViewController {
     /// the name of the list view template
     var listView: String { get }
-    
-    var listOrderByKey: String { get }
+    /// url query parameter list order key
+    var listOrderKey: String { get }
+    /// url query parameter list sort key
     var listSortKey: String { get }
-    
+    /// url query parameter list search key
     var listSearchKey: String { get }
+    /// url query parameter list limit key
     var listLimitKey: String { get }
+    /// url query parameter list page key
     var listPageKey: String { get }
 
-    var listOrderBy: [FieldKey] { get }
-    var listSort: Sort { get }
-    var listLimit: Int { get }
+    /// returns allowed order by fields if empty list can't be ordered
+    var listAllowedOrders: [FieldKey] { get }
+    /// default sort direction
+    var listDefaultSort: Sort { get }
+    /// default list limit
+    var listDefaultLimit: Int { get }
 
     /// builds the query in order to list objects in the admin interface
     func beforeList(req: Request, queryBuilder: QueryBuilder<Model>) throws -> QueryBuilder<Model>
  
+    /// search
     func search(using: QueryBuilder<Model>, for: String)
+    
+    /// renders the list view
     func listView(req: Request) throws -> EventLoopFuture<View>
 
+    /// setup list related routes
     func setupListRoute(on: RoutesBuilder)
 }
 
 public extension ListViewController {
 
-    var listOrderByKey: String { "order" }
+    var listOrderKey: String { "order" }
     var listSortKey: String { "sort" }
-
     var listSearchKey: String { "search" }
     var listLimitKey: String { "limit" }
     var listPageKey: String { "page" }
 
-    var listOrderBy: [FieldKey] { [] }
-    var listSort: Sort { .asc }
-    var listLimit: Int { 10 }
-
+    var listAllowedOrders: [FieldKey] { [] }
+    var listDefaultSort: Sort { .asc }
+    var listDefaultLimit: Int { 10 }
+    
     func search(using qb: QueryBuilder<Model>, for searchTerm: String) {}
 
     func beforeList(req: Request, queryBuilder: QueryBuilder<Model>) throws -> QueryBuilder<Model> {
@@ -50,40 +59,49 @@ public extension ListViewController {
     }
 
     func listView(req: Request) throws -> EventLoopFuture<View> {
-        let customOrder: String? = req.query[listSortKey]
-        var sortValue = listSort
-        if let i: String = req.query[listSortKey], let o = Sort(rawValue: i) {
-            sortValue = o
-        }
-        let searchString: String? = req.query[listSearchKey]
-        let limit: Int = req.query[listLimitKey] ?? listLimit
-        let page: Int = max((req.query[listPageKey] ?? 1), 1)
-
+        /// first we need a QueryBuilder instance, we apply the beforeList method on the default query
         var qb = try beforeList(req: req, queryBuilder: Model.query(on: req.db))
-        
-        if customOrder != nil || !listOrderBy.isEmpty {
-            let orderValue = customOrder ?? listOrderBy[0].description
-            qb = qb.sort(.init(stringLiteral: orderValue), sortValue.direction)
+
+        /// next we get the sort from the query, if there was no sort key we use the default sort
+        var sort = listDefaultSort
+        if let sortQuery: String = req.query[listSortKey], let sortValue = Sort(rawValue: sortQuery) {
+            sort = sortValue
+        }
+        /// we also use apply the custom order if it is allowed to order the list
+        if !listAllowedOrders.isEmpty, let orderValue: String = req.query[listOrderKey] {
+            let order = FieldKey(stringLiteral: orderValue)
+            /// only allow ordering if the order value is in the allowed orders array
+            if listAllowedOrders.contains(order) {
+                qb = qb.sort(order, sort.direction)
+            }
         }
 
-        if let searchTerm = searchString, !searchTerm.isEmpty {
+        /// check if there is a non-empty search term and apply the search term using the custom search method
+        if let searchTerm: String = req.query[listSearchKey], !searchTerm.isEmpty {
             qb = qb.group(.or) { search(using: $0, for: searchTerm) }
         }
 
+        /// apply the limit and page properties
+        let limit: Int = req.query[listLimitKey] ?? listDefaultLimit
+        let page: Int = max((req.query[listPageKey] ?? 1), 1)
+        
+        /// calculate the start and end position
         let start: Int = (page - 1) * limit
         let end: Int = page * limit
         
+        /// count the total number of elements for the page info
         let count = qb.count()
+        /// set the range filter and request all the elements in the given range
         let items = qb.copy().range(start..<end).all()
-
+        
+        ///query both the total count and the models for the requested page
         return items.and(count).map { (models, total) -> Page<Model> in
             let totalPages = Int(ceil(Float(total) / Float(limit)))
             return Page(models, info: .init(current: page, limit: limit, total: totalPages))
         }
+        /// map the page elements to Leaf values & render the list view
         .map { $0.map(\.leafData) }
-        .flatMap {
-            req.leaf.render(template: listView, context: ["list": $0.leafData])
-        }
+        .flatMap { req.leaf.render(template: listView, context: ["list": $0.leafData]) }
     }
 
     func setupListRoute(on builder: RoutesBuilder) {
